ab3aaf0960feda449291e7686ad44882524cb712
João Paulo Gobira <jpgobira2@gmail.com>
2026-06-12 11:24:25 -0300

    fix: imports

--- src/main/java/br/pucminas/hospedagem/controller/AluguelController.java
+++ src/main/java/br/pucminas/hospedagem/controller/AluguelController.java
@@ -1,11 +1,20 @@
 package br.pucminas.hospedagem.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.pucminas.hospedagem.model.Aluguel;
 import br.pucminas.hospedagem.service.AluguelService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/alugueis")
